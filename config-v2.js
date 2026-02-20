e3964d277f4ab25a1c32e1b4e7876ba358ecd0ff
pathummalsara93-ux <pathummalsara93@gmail.com>
2026-02-20 08:45:24 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94788119688" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~dOO1601N7LmzwRf" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
